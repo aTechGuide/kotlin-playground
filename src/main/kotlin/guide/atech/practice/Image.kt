50768449d632d2c9de1d5966c51e3399b8489224
Kamran Ali <kamranalinitb@gmail.com>
2024-03-19 16:40:31 +0530

    Exercise on Cropping Image

---
+++ src/main/kotlin/guide/atech/practice/Image.kt
@@ -0,0 +1,62 @@
+package guide.atech.practice
+
+import java.awt.Graphics
+import java.awt.Window
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+
+// crop a picture
+class Image(val buffImage: BufferedImage) {
+
+    val width = buffImage.width
+    val height = buffImage.height
+
+    fun save(path: String) =
+        ImageIO.write(buffImage, "JPG", File(path))
+
+    fun saveResource(path: String) =
+        save("src/main/resources/$path")
+
+    fun crop(startX: Int, startY: Int, w: Int, h: Int): Image? {
+        // dimension checks
+        if (startX < 0 || startX >= width || startY < 0 || startY >= height) return null
+        if (w < 0 || startX + w >= width || h < 0 || startY + h >= height) return null
+
+        // happy path
+        val result = Image.black(w, h)
+        for (x in startX ..< (startX + w))
+            for (y in startY ..< (startY + h)) {
+                val originalPixel = buffImage.getRGB(x,y)
+                result.buffImage.setRGB(x - startX, y - startY, originalPixel)
+            }
+
+        return result
+    }
+
+
+    companion object {
+
+        fun black(width: Int, height: Int): Image {
+            val buffImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+            val pixels = IntArray(width * height) { 0 }
+            buffImage.setRGB(0,0,width,height,pixels,0,width)
+            return Image(buffImage)
+        }
+
+        fun load(path: String) =
+            Image(ImageIO.read(File(path)))
+
+        fun loadResource(path: String) =
+            load("src/main/resources/$path")
+    }
+
+}
+
+object ImagePlayground {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        //Image.black(100, 100).saveResource("black.jpg")
+        Image.loadResource("wikicrop.jpg").crop(480, 180, 350, 180)?.saveResource("cropped.jpg")
+    }
+}
